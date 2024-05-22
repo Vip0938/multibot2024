e720d5d18f5ecfca4b570d4d20a32dbb9a65a5ff
Vip0938 <75055638+Vip0938@users.noreply.github.com>
2024-05-21 23:35:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  OWNER_IDS: [""], // Bot owner ID's
-  SUPPORT_SERVER: "", // Your bot support server
+  OWNER_IDS: ["640015277156991005"], // Bot owner ID's
+  SUPPORT_SERVER: "https://discord.gg/ab8hr2yj9n", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
     DEFAULT_PREFIX: "!", // Default prefix for the bot
